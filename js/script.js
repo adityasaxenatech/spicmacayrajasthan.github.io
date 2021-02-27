8f0808c6f8822c83b0cfcc715834611423c52667
Aditya Saxena <adityasaxenamail@gmail.com>
2021-02-27 19:25:09 +0530

    newspicmacay

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-function init(){
-    document.querySelector('#jewel-1-main').style.display='none';
+// const inpFile = document.getElementById("inpFile");
+
+// const imgContainer = document.getElementById("img-Prev");
+
+// const img = imgContainer.querySelector(".img");
+
+// inpFile.addEventListener("change" , function(){
+//     const file = this.files[0];
+
+//     if(file){
+//         const reader = new FileReader();
+
+//         img.style.display = "block";
+
+//         reader.addEventListener("load" , function(){
+//             console.log(this);
+//             img.setAttribute("src", this.result);
+//         });
+//         reader.readAsDataURL(file);
+//     }
+
+// });
+
+
+function showPreview(event){
+if(event.target.files.length > 0){
+var src = URL.createObjectURL(event.target.files[0]);
+var preview = document.getElementById("slide-img");
+preview.src = src;
+preview.style.display = "block";
 }
-
-init();
-
-document.querySelector('.btn').addEventListener('click', function(){
-    document.querySelector('#jewel-1-main').style.display='block';
-});
+}
